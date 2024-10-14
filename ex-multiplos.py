6e078ebc868d7b89ced9b365e4565a9a95fd1ff1
Nathalia Ferreira <nathalia.fdsousa@gmail.com>
2024-10-14 14:33:32 -0300

    Add files via upload

---
+++ ex-multiplos.py
@@ -0,0 +1,9 @@
+contador = 0
+atual = 3
+multiplos = []
+while contador <= 10:
+        atual += 3
+        multiplos.append(atual)
+        contador += 1
+       
+print(f'Os 10 primeiros míltiplos de 3 são: {multiplos}')
