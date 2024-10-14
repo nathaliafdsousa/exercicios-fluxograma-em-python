50385dba784b62d638c7abc678f5d6e3ed02842a
Nathalia Ferreira <nathalia.fdsousa@gmail.com>
2024-10-14 18:41:44 -0300

    Atualizar o ex=imapres.py

--- ex=imapres.py
+++ ex=imapres.py
@@ -2,6 +2,6 @@ i = 1
 while i <= 99:
     if i % 2 != 0:
         print(f'O número ímpar é {i}')
-    i += 2  # Corrigido para incrementar 'i'
+    i += 2  
 
 print(f'Os números ímpares de 1 a 99 são: {list(range(1, 100, 2))}')
